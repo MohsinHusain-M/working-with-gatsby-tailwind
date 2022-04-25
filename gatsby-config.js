51312fb5c4dac7a0a5061436253b9c31bcf2b3b0
MohsinHusain-M <66244428+MohsinHusain-M@users.noreply.github.com>
2022-04-25 11:30:40 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: `Ednam-solutions-v2`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "wSVnIeuGGODrmvYLI7POCgyTTZBgNhkl3X9Ctw4yFow",
+      "spaceId": "4yn1w4iyl62m"
+    }
+  }]
+};
