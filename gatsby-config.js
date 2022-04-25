de70a94e31b32d92c1cc4547365ee391a767e272
MohsinHusain-M <66244428+MohsinHusain-M@users.noreply.github.com>
2022-04-25 11:50:22 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,19 @@
 module.exports = {
   siteMetadata: {
     title: `Ednam-solutions-v2`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "wSVnIeuGGODrmvYLI7POCgyTTZBgNhkl3X9Ctw4yFow",
-      "spaceId": "4yn1w4iyl62m"
-    }
-  }]
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "wSVnIeuGGODrmvYLI7POCgyTTZBgNhkl3X9Ctw4yFow",
+        spaceId: "4yn1w4iyl62m",
+      },
+    },
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
